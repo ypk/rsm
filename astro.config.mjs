45ab9782cc201fb8234d89b770dea3b15df938a3
ypk <yprasannakumar@gmail.com>
2025-09-25 10:31:57 +0100

    v2 updated astro config to fix site and base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ypk.github.io',
-  base: '/',
+  base: '/rsm',
   vite: {
     plugins: [tailwindcss()],
     server: {
