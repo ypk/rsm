886bec99026f81e1d7b95bbcdc4bdebfd6b7894b
ypk <yprasannakumar@gmail.com>
2025-09-25 11:39:42 +0100

    updated base url in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ypk.github.io',
-  base: '/rsm',
+  base: '/rsm/',
   vite: {
     plugins: [tailwindcss()],
     server: {
